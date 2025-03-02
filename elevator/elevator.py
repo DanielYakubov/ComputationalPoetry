986181e4d8644d4ca1cb15b038dd9c1944ec0693
DanielYakubov <Danielyak98@gmail.com>
2025-03-02 15:16:29 -0500

    moving elevator to a dedicated folder with minor improvements (p2)

--- elevator/elevator.py
+++ elevator/elevator.py
@@ -1,16 +1,15 @@
 import time
 
-# Mark Strand, "Elevator"
-STANZA = """The elevator went to the basement. The doors opened.
-A man stepped in and asked if I was going up.
-\"I’m going down,\" I said. \"I won’t be going up.\""""
+# from Mark Strand, "Elevator"
+with open("stanza.txt", "r") as f:
+    STANZA = f.read()
 INTERVAL = 14.69 / 1000
 STANZA_NUM = 1
 
 while True:
     print(STANZA_NUM)
     for c in STANZA:
-        print(c, end='')
+        print(c, end='', flush=True)
         time.sleep(INTERVAL)
     print('\n')
     STANZA_NUM += 1
